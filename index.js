7598f7a247d70c4885e5963c0644f300849a872a
pb-totoro <jonesec2@gmail.comgit config --global user.email jonesec2@gmail.com>
2020-01-21 18:35:08 -0500

    Added some prompts

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 const inquirer = require("inquirer")
 const jest = require("jest")
 const fs = require("jf")
+
+function userPrompts() {
+    return inquirer.prompt([
+        {
+            message: "What is the employee's name?",
+            name: "name"
+        },
+        {
+            message: "What is the employee's role?",
+            name: "role"
+        },
+        {
+            message: "What is employee's email?",
+            name: "email"
+        },
+        {
+            message: "What is employee's Id?",
+            name: "id"
+        },
+        {
+            message: ""
+        }
+    ])
+} 
