827084f9b427d1a5dee88878ddda58feda45d4e2
pb-totoro <jonesec2@gmail.com>
2020-01-20 21:07:25 -0500

    Created employee class template

---
+++ employee.js
@@ -0,0 +1,27 @@
+class Employee {
+    constructor(name, id , title) {
+    this.name = name;
+    this.id = id;
+    this.title = title;
+    }
+
+    // get name function
+    getName(){
+
+    }
+
+    // get employee by ID
+    getId(){
+
+    }
+
+    // get employees email
+    getEmail() {
+
+    }
+
+    // returns "Employee"
+    getRole(){
+
+    }
+}
